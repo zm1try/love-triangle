0bf1565363a012153d9276b02c1243c732cf729b
zm1try <zmitryarosh@gmail.com>
2018-02-21 16:29:10 +0300

    100/100

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  this.p = preferences;
+  var count = 0;
+  for(let i = 0; i<p.length; i++) {
+    if (p[p[p[i]-1]-1]-1 === i && (p[i]-1) !== i && (p[p[i]-1]-1) !== (p[i]-1)) count++;
+  }
+  return count/3;
 };
